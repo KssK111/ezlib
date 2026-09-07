8914b3bb918ae63248f9ea78a0915a50465e3e50
Szymon <kssk12jogurt@gmail.com>
2026-09-07 09:34:33 +0200

    Init commit

---
+++ ezstr.h
@@ -0,0 +1,17 @@
+#define DEV 1
+#if DEV
+#define EZSTR_IMPL
+#endif
+
+#ifndef EZSTR_H
+#define EZSTR_H
+
+// Definition
+
+#ifdef EZSTR_IMPL
+
+// Implementation
+
+#endif // EZSTR_IMPL
+
+#endif // !EZSTR_H
