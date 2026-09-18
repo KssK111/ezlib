bcddfe2af95c7681c2bfb289dab28643ddb86d49
Szymon <kssk12jogurt@gmail.com>
2026-09-18 08:17:59 +0200

    remove inline deps

--- README.md
+++ README.md
@@ -2,10 +2,12 @@
 
 ### How to use **Header-only libraries**
 HOLs are self-contained, this means you can just include a single file into your project and it should work\
+(However, for better code organisation libraries in this repo are not self-contained, dependencies are listed on the top of the files)
 Not to get a linker error, you must also define an implementation macro, because HOLs only include the declarations by default\
 This is because of the *One Definition Rule*, not to break the rule, you must only define the implementation macro once\
 It will probably be in your main.cpp file, **before the inclusion of the header**\
 In other files, include the header, without defining the macro
+If you still get errors, try including just the header on the top of the file and implementation on the bottom
 
 ```cpp
 // main.cpp
--- ezstr.h
+++ ezstr.h
@@ -1,6 +1,8 @@
 #ifndef EZSTR_H
 #define EZSTR_H
 
+#include "ezoption.h"
+
 #include <algorithm>
 #include <cassert>
 #include <cctype>
@@ -31,44 +33,6 @@
  */
 
 /* --------------- Declaration + Templates --------------- */
-
-/* --------------- Deps --------------- */
-/* --------------- Option --------------- */
-#include <functional>
-#include <utility>
-
-#ifndef EZOPTION_H
-#define EZOPTION_H
-namespace ezopt {
-
-template <typename T> class Option;
-template <typename T> class Option {
-  bool is_some_;
-  union {
-    T value_;
-  };
-
-public:
-  Option() : is_some_(false) {}
-  Option(T &&value) : is_some_(true), value_(std::move(value)) {}
-  ~Option() {
-    if (is_some_)
-      value_.~T();
-  }
-  [[nodiscard]] explicit operator bool() const { return is_some_; }
-  [[nodiscard]] operator T() && { return std::move(value_); }
-  [[nodiscard]] T unwrap_or(T &&substitute) && {
-    return is_some_ ? std::move(value_) : std::move(substitute);
-  }
-  [[nodiscard]] T unwrap_or_else(std::function<T()> fn) && {
-    return is_some_ ? std::move(value_) : fn();
-  }
-};
-
-} // namespace ezopt
-#endif // !EZOPTION_H
-/* --------------- /Deps --------------- */
-
 namespace ezstr {
 
 class String;
